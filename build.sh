e47441557e77a97f55b3a74f9fbd2fa4a6a34289
Toon Spin <toon.spin@gmail.com>
2019-12-05 00:15:18 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in $(seq -w 26); do
+    if [ -e "src/day${i}.ps" ]; then
+        gs \
+            -dDEVICEWIDTHPOINTS=595 -dDEVICEHEIGHTPOINTS=832 \
+            -dBATCH \
+            -sDEVICE=pdfwrite -sOutputFile="output/day${i}.pdf" \
+            "data/day${i}.txt" "src/day${i}.ps"
+    fi
+done
